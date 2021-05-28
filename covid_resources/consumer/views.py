df41c10fa05ea2b127297795c8190e75b5a6b352
Kajal <kajalsahu0028@gmail.com>
2021-05-28 23:23:04 +0530

    created models

--- covid_resources/consumer/views.py
+++ covid_resources/consumer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# def index(request):
+#     return render(request, 'provider_form.html')
+
+
+def index(request):
+    return render(request, 'home.html')
+
+def tnc(request):
+    return render(request, 't&c.html')
